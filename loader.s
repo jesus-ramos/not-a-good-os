3875b34c67ebc9711006d4d0d7bb16166507a94f
Jesus Ramos <jesus-ramos@live.com>
2012-04-20 03:31:26 -0400

    Who needs comments anyways?

--- loader.s
+++ loader.s
@@ -1,38 +1,36 @@
-global loader                           ; making entry point visible to linker
- 
-extern kmain                            ; kmain is defined in kernel.c
- 
-; setting up the Multiboot header - see GRUB docs for details
-MODULEALIGN equ  1<<0                   ; align loaded modules on page boundaries
-MEMINFO     equ  1<<1                   ; provide memory map
-FLAGS       equ  MODULEALIGN | MEMINFO  ; this is the Multiboot 'flag' field
-MAGIC       equ    0x1BADB002           ; 'magic number' lets bootloader find the header
-CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum required
- 
+global loader
+
+extern kmain
+
+MODULEALIGN equ  1<<0
+MEMINFO     equ  1<<1
+FLAGS       equ  MODULEALIGN | MEMINFO
+MAGIC       equ    0x1BADB002
+CHECKSUM    equ -(MAGIC + FLAGS)
+
 section .text
- 
+
 align 4
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
- 
-; reserve initial kernel stack space
-STACKSIZE equ 0x4000                    ; that's 16k.
- 
+
+STACKSIZE equ 0x4000
+
 loader:
-    mov  esp, stack + STACKSIZE         ; set up the stack
-    push eax                            ; Multiboot magic number
-    push ebx                            ; Multiboot info structure
- 
-    call kmain                          ; call kernel proper
- 
+    mov  esp, stack + STACKSIZE
+    push eax
+    push ebx
+
+    call kmain
+
     cli
 .hang:
-    hlt                                 ; halt machine should kernel return
+    hlt
     jmp  .hang
- 
+
 section .bss
- 
+
 align 4
 stack:
-    resb STACKSIZE                      ; reserve 16k stack on a doubleword boundary
+    resb STACKSIZE
