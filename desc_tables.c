53060cf61f9d75df529b79705c77df57d0133b22
Jesus Ramos <jesus-ramos@live.com>
2012-04-26 16:27:59 -0400

    Wrapping idt_set_gate in a nicer macro for now

--- desc_tables.c
+++ desc_tables.c
@@ -11,6 +11,8 @@ struct gdt_ptr gdt_ptr;
 struct idt_entry idt_entries[256];
 struct idt_ptr idt_ptr;
 
+#define IDT_SET_GATE(isrnum) idt_set_gate(isrnum, (uint32_t)isr##isrnum, 0x08, 0x8E)
+
 static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit,
                          uint8_t access, uint8_t granularity)
 {
@@ -56,42 +58,41 @@ static void init_idt()
 
     memset(&idt_entries, 0, sizeof(struct idt_entry) * 256);
 
-    idt_set_gate( 0, (uint32_t)isr0 , 0x08, 0x8E);
-    idt_set_gate( 1, (uint32_t)isr1 , 0x08, 0x8E);
-    idt_set_gate( 2, (uint32_t)isr2 , 0x08, 0x8E);
-    idt_set_gate( 3, (uint32_t)isr3 , 0x08, 0x8E);
-    idt_set_gate( 4, (uint32_t)isr4 , 0x08, 0x8E);
-    idt_set_gate( 5, (uint32_t)isr5 , 0x08, 0x8E);
-    idt_set_gate( 6, (uint32_t)isr6 , 0x08, 0x8E);
-    idt_set_gate( 7, (uint32_t)isr7 , 0x08, 0x8E);
-    idt_set_gate( 8, (uint32_t)isr8 , 0x08, 0x8E);
-    idt_set_gate( 9, (uint32_t)isr9 , 0x08, 0x8E);
-    idt_set_gate(10, (uint32_t)isr10, 0x08, 0x8E);
-    idt_set_gate(11, (uint32_t)isr11, 0x08, 0x8E);
-    idt_set_gate(12, (uint32_t)isr12, 0x08, 0x8E);
-    idt_set_gate(13, (uint32_t)isr13, 0x08, 0x8E);
-    idt_set_gate(14, (uint32_t)isr14, 0x08, 0x8E);
-    idt_set_gate(15, (uint32_t)isr15, 0x08, 0x8E);
-    idt_set_gate(16, (uint32_t)isr16, 0x08, 0x8E);
-    idt_set_gate(17, (uint32_t)isr17, 0x08, 0x8E);
-    idt_set_gate(18, (uint32_t)isr18, 0x08, 0x8E);
-    idt_set_gate(19, (uint32_t)isr19, 0x08, 0x8E);
-    idt_set_gate(20, (uint32_t)isr20, 0x08, 0x8E);
-    idt_set_gate(21, (uint32_t)isr21, 0x08, 0x8E);
-    idt_set_gate(22, (uint32_t)isr22, 0x08, 0x8E);
-    idt_set_gate(23, (uint32_t)isr23, 0x08, 0x8E);
-    idt_set_gate(24, (uint32_t)isr24, 0x08, 0x8E);
-    idt_set_gate(25, (uint32_t)isr25, 0x08, 0x8E);
-    idt_set_gate(26, (uint32_t)isr26, 0x08, 0x8E);
-    idt_set_gate(27, (uint32_t)isr27, 0x08, 0x8E);
-    idt_set_gate(28, (uint32_t)isr28, 0x08, 0x8E);
-    idt_set_gate(29, (uint32_t)isr29, 0x08, 0x8E);
-    idt_set_gate(30, (uint32_t)isr30, 0x08, 0x8E);
-    idt_set_gate(31, (uint32_t)isr31, 0x08, 0x8E);
+    IDT_SET_GATE(0);
+    IDT_SET_GATE(1);
+    IDT_SET_GATE(2);
+    IDT_SET_GATE(3);
+    IDT_SET_GATE(4);
+    IDT_SET_GATE(5);
+    IDT_SET_GATE(6);
+    IDT_SET_GATE(7);
+    IDT_SET_GATE(8);
+    IDT_SET_GATE(9);
+    IDT_SET_GATE(10);
+    IDT_SET_GATE(11);
+    IDT_SET_GATE(12);
+    IDT_SET_GATE(13);
+    IDT_SET_GATE(14);
+    IDT_SET_GATE(15);
+    IDT_SET_GATE(16);
+    IDT_SET_GATE(17);
+    IDT_SET_GATE(18);
+    IDT_SET_GATE(19);
+    IDT_SET_GATE(20);
+    IDT_SET_GATE(21);
+    IDT_SET_GATE(22);
+    IDT_SET_GATE(23);
+    IDT_SET_GATE(24);
+    IDT_SET_GATE(25);
+    IDT_SET_GATE(26);
+    IDT_SET_GATE(27);
+    IDT_SET_GATE(28);
+    IDT_SET_GATE(29);
+    IDT_SET_GATE(30);
+    IDT_SET_GATE(31);
 
     idt_flush((uint32_t)&idt_ptr);
 }
-
 
 void init_descriptor_tables()
 {
